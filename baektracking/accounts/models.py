4334bc6d4f5482e66e26b04d4935354191ff0a0c
sungyujeon <sungyujeon91@gmail.com>
2021-03-27 13:32:20 +0900

    delete incorrect pjt folder

--- baektracking/accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.conf import settings
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    baekjoon_id = models.CharField(max_length=100)
-    profile_image = models.ImageField(blank=True, null=True)
--- baektracking/baektracking/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.urls import path
-
-def home(request):
-    return render(request, 'home.html')
