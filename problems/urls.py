d94ae2be738c8a567cc444869bb91faea4b5776f
sungyujeon <sungyujeon91@gmail.com>
2021-04-08 21:01:33 +0900

    update urls and views in problems app
    1. 한 유저가 푼 문제 크롤링하기 작업중
      - urls.py, views.py 수정
    2. accounts app의 profile.html 수정
      - profile.html에서 한 유저가 푼 문제 크롤링 버튼 생성중

--- problems/urls.py
+++ problems/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'problems'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('problems/crawl_users', views.crawl_all_users_solved_problems, name='crawl_all_users_solved_problems'),
+    path('crawl_users/', views.crawl_all_users_solved_problems, name='crawl_all_users_solved_problems'),
+    path('crawl_user/<baekjoon_id>/', views.crawl_a_user_solved_problems, name='crawl_a_user_solved_problems'),
+
 ]
